c85d34b6f65ce9ec423dfc2fc55c3d10d6c940ac
NRGLook <ilya.tihanyonok@yandex.by>
2024-01-29 00:12:29 +0300

    Add cipher Caesar and Vigenere text files

--- Lab1_Caesar_and_Vigenère_cipher.py
+++ Lab1_Caesar_and_Vigenère_cipher.py
@@ -32,6 +32,23 @@ def vigenere_cipher(text, key):
     return encrypted_text
 
 
+def vigenere_decipher(text, key):
+    key_length = len(key)
+    decrypted_text = ""
+    for i, char in enumerate(text):
+        if char.isalpha():
+            key_shift = string.ascii_lowercase.index(key[i % key_length].lower())
+            shift = string.ascii_lowercase.index(char.lower()) - key_shift
+            shifted_index = shift % 26
+            if char.islower():
+                decrypted_text += string.ascii_lowercase[shifted_index]
+            else:
+                decrypted_text += string.ascii_uppercase[shifted_index]
+        else:
+            decrypted_text += char
+    return decrypted_text
+
+
 def encrypt_file_caesar(input_file, output_file, shift):
     with open(input_file, 'r') as file:
         plaintext = file.read()
@@ -59,7 +76,7 @@ def encrypt_file_vigenere(input_file, output_file, key):
 def decrypt_file_vigenere(input_file, output_file, key):
     with open(input_file, 'r') as file:
         ciphertext = file.read()
-    decrypted_text = vigenere_cipher(ciphertext, key)
+    decrypted_text = vigenere_decipher(ciphertext, key)
     with open(output_file, 'w') as file:
         file.write(decrypted_text)
 
